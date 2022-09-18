7c9c3d9b35c222d3a4798c7d0ead0100ed642d5d
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-19 01:12:15 +0900

    HelloMiddlewareを修正する p111

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -15,6 +15,12 @@ class HelloMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $data = [
+            ['name' => 'taro', 'mail' => 'taro@yamada'],
+            ['name' => 'hanako', 'mail' => 'hanako@flower'],
+            ['name' => 'sachiko', 'mail' => 'sachico@happy'],
+        ];
+        $request->merge(['data' => $data]);
         return $next($request);
     }
 }
