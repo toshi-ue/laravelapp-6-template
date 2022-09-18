927bca7ce6e29f4bd1df0f58b69e6f2e8c45de0c
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-18 14:58:15 +0900

    クロージャでコンポーザ処理を作る p101

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class HelloServiceProvider extends ServiceProvider
@@ -23,6 +24,11 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer(
+            'hello.index',
+            function ($view) {
+                $view->with('view_message', 'composer message!');
+            }
+        );
     }
 }
