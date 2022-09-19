eac6f9d4f362606a27fcf724178efce3be46938c
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-19 12:47:04 +0900

    バリデータを作成する p144

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-
+use Validator;
 
 class HelloController extends Controller
 {
@@ -14,8 +14,18 @@ class HelloController extends Controller
         return view('hello.index', ['msg' => 'フォームを入力：']);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
         return view('hello.index', ['msg' => '正しく入力されました！']);
     }
 }
