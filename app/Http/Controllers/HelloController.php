f19d17d1e2fc460c0abbf10e82ae7aa8bd2e5ca1
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-20 04:06:22 +0900

    DBクラスとは p192

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,18 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Validator;
+
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->hasCookie('msg')) {
-            $msg = 'Cookie: ' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーはありません。';
-        }
-        return view('hello.index', ['msg' => $msg]);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
 
     public function post(Request $request)
