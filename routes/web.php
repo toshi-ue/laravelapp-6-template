cbf84e3cb61d125e1a47eb98109884fb96160ee3
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-20 15:30:23 +0900

    index.blade.phpを作成する p240

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,4 @@ Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
 Route::get('hello/show', 'HelloController@show');
+Route::get('person', 'PersonController@index');
