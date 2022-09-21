2d23c700c9bd9e93b38623e5e9d343bb55646d95
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-21 17:15:17 +0900

    モデルを修正する p256

--- app/Person.php
+++ app/Person.php
@@ -8,30 +8,16 @@ use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
-    protected static function boot()
-    {
-        parent::boot();
+    protected $guarded = array('id');
 
-        static::addGlobalScope(new ScopePerson);
-    }
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150',
+    );
 
     public function getData()
     {
         return $this->id . ': ' . $this->name . '(' . $this->age . ')';
-    }
-
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
-
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
-
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
     }
 }
