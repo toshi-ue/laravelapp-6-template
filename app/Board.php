67ffd64d34e1c496da4ad9bd81ff62ed078532f3
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-22 22:43:04 +0900

    belongs To結合 p282

--- app/Board.php
+++ app/Board.php
@@ -16,6 +16,11 @@ class Board extends Model
 
     public function getData()
     {
-        return $this->id . ': ' . $this->title;
+        return $this->id . ': ' . $this->title . ' (' . $this->person->name . ')';
+    }
+
+    public function person()
+    {
+        return $this->belongsTo('App\Person');
     }
 }
