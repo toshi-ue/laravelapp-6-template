c6305d6b1ba8cd05604e20d4b0234ac427d9ba2e
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-23 16:43:55 +0900

    フォームを/hello/restに埋め込む p302

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
 Route::get('hello/show', 'HelloController@show');
 Route::get('person', 'PersonController@index');
+Route::get('hello/rest', 'HelloController@rest');
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
 Route::get('person/add', 'PersonController@add');
