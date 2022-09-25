d8198c4c11646f8f78bfd757b45bfbf75b54da08
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-25 20:49:35 +0900

    DBクラスとsimplePaginate p313

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\HelloRequest;
+use App\Person;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
@@ -13,7 +14,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::table('people')->orderBy('age', 'asc')->get();
+        $items = DB::table('people')->simplePaginate(5);
         return view('hello.index', ['items' => $items]);
     }
 
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.helloapp')
+<style>
+    .pagination {
+        font-size: 10pt;
+    }
 
+    .pagination li {
+        display: inline-block
+    }
+</style>
 @section('title', 'Index')
 
 @section('menubar')
@@ -8,19 +16,22 @@
 @endsection
 
 @section('content')
-    <table>
-    <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Mail</th>
+        <th>Age</th>
+    </tr>
     @foreach ($items as $item)
-        <tr>
-            <td>{{$item->name}}</td>
-            <td>{{$item->mail}}</td>
-            <td>{{$item->age}}</td>
-        </tr>
+    <tr>
+        <td>{{$item->name}}</td>
+        <td>{{$item->mail}}</td>
+        <td>{{$item->age}}</td>
+    </tr>
     @endforeach
-    </table>
+</table>
+{{ $items->links() }}
 @endsection
-
-
 
 @section('footer')
 copyright 2020 tuyano.
