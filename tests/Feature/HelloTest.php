b13cb28dd466140cdacae69930d8c6ab1c716b0b
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-09-27 22:13:46 +0900

    指定アドレスにアクセスする p343

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 // TODO: なぜこれが必要なのか?そしてなぜもともとあったコードを削除したのか?
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -10,17 +11,23 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class HelloTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function testHello()
     {
         $this->assertTrue(true);
 
-        $arr = [];
-        $this->assertEmpty($arr);
+        $response = $this->get('/');
+        $response->assertStatus(200);
 
-        $msg = "Hello";
-        $this->assertEquals('Hello', $msg);
+        $response = $this->get('/hello');
+        $response->assertStatus(302);
 
-        $n = random_int(0, 100);
-        $this->assertLessThan(100, $n);
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('/hello');
+        $response->assertStatus(200);
+
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
     }
 }
